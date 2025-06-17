e471bc3fb59413af1cd9df2e2ed4ff3e87275e6d
martinpercu <97993590+martinpercu@users.noreply.github.com>
2025-06-17 19:54:00 -0400

    implemented lang switcher

---
+++ src/app/shared/lang-switcher/lang-switcher.component.ts
@@ -0,0 +1,29 @@
+import { Component, inject } from '@angular/core';
+import { LangService } from '@services/lang.service';
+
+@Component({
+  selector: 'app-lang-switcher',
+  imports: [],
+  templateUrl: './lang-switcher.component.html',
+  styleUrl: './lang-switcher.component.css',
+})
+export class LangSwitcherComponent {
+  langService = inject(LangService);
+
+  currentLang!: string;
+
+  constructor() {
+    this.currentLang = this.langService.getLang();
+    console.log(this.currentLang);
+  }
+
+  changeLang(lang: string) {
+    this.langService.setLang(lang);
+    this.currentLang = lang
+  }
+
+  action() {
+    const caca = this.langService.traductor('welcome');
+    console.log(caca);
+  }
+}
