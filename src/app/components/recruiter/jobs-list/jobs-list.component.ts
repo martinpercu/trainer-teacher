0d70e1c76cd70229700d61a88622645c1e3955df
martinpercu <97993590+martinpercu@users.noreply.github.com>
2025-07-15 11:24:39 -0400

    order in the joblist component

--- src/app/components/recruiter/jobs-list/jobs-list.component.ts
+++ src/app/components/recruiter/jobs-list/jobs-list.component.ts
@@ -1,13 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { TranslocoPipe } from '@jsverse/transloco';
-
-
-// import { Student } from '@models/student';
 import { MatIconModule } from '@angular/material/icon';
 
 import { Job } from '@models/job';
 import { Candidate } from '@models/candidate';
-import { Result } from '@models/result';
 
 @Component({
   selector: 'app-jobs-list',
@@ -16,17 +12,67 @@ import { Result } from '@models/result';
   styleUrl: './jobs-list.component.css'
 })
 export class JobsListComponent {
-
   @Input() job!: Job;
   @Input() candidatesForJob!: Candidate[];
-  @Input() results!: Result[]
+  // @Input() results!: Result[];
+  @Input() candidatesForJobAndExamPassed!: Candidate[];
 
+  magicLink: string = 'Este es el texto super importante que ya tengo en mi variable.';
+  copiedSucces: boolean = false; // Para mostrar un mensaje de éxito
+  errorInCopy: boolean = false; // Para mostrar un mensaje de error
+
+  showCandidates!: boolean;
+  showCandidatesExamPassed!: boolean;
 
   ngOnInit() {
+    console.log('START OnINIT Job-List');
     console.log(this.job);
     console.log(this.candidatesForJob);
-    console.log(this.results);
-  }
+    console.log(this.candidatesForJobAndExamPassed);
+    this.showCandidates = false
+    this.showCandidatesExamPassed = false
+    this.magicLink = "http://localhost:4200/job/"+this.job.jobId;
+    console.log(this.magicLink);
+    console.log('END OnINIT Job-List');
+  };
 
+  switchShowCandidates(){
+    this.showCandidates = !this.showCandidates
+  };
+
+  switchShowCandidatesExamPassed(){
+    this.showCandidatesExamPassed = !this.showCandidatesExamPassed
+  };
+
+  async copyMagicString(): Promise<void> {
+    this.copiedSucces = false;
+    this.errorInCopy = false;
+    // Check if API Clipboard is ready on Browser
+    if (navigator.clipboard && navigator.clipboard.writeText) {
+      try {
+        // Intentar escribir el texto en el portapapeles
+        await navigator.clipboard.writeText(this.magicLink);
+        console.log('String copiado exitosamente al portapapeles:', this.magicLink);
+        this.copiedSucces = true;
+        // hide succes message after 2 secs.
+        setTimeout(() => {
+          this.copiedSucces = false;
+        }, 2000);
+      } catch (err) {
+        // Catch error if copy problem (ej. denied permits)
+        console.error('Error trying copy:', err);
+        this.errorInCopy = true;
+        // hide error message after 3 secs
+        setTimeout(() => {
+          this.errorInCopy = false;
+        }, 3000);
+      }
+    } else {
+      // Show warning if API is not available (very old browsers)
+      console.warn('API Clipboard not compatible with this browser.');
+      this.errorInCopy = true; // Podrías usar un mensaje diferente aquí si quieres
+      alert('Tu navegador no soporta la copia automática. Por favor, copia manualmente el texto: \n\n' + this.magicLink);
+    }
+  }
 
 }
